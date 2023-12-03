00e7f72a7f0118bd0a89d9a63524be9cc6b5f208
joaomacp <95joaopereira@gmail.com>
2023-12-03 02:16:02 +0000

    days 1 and 2

---
+++ day01/day01.py
@@ -0,0 +1,54 @@
+## Input
+with open("input/day01.txt") as f:
+  input = f.read()
+  input_lines = input.splitlines()
+
+
+## Part 1
+def find_digit_part_one(char_iterator):
+  for char in char_iterator:
+    if char.isdigit():
+      return char
+
+result_part_one = 0
+
+for line in input_lines:
+  first_digit = find_digit_part_one(line)
+  last_digit = find_digit_part_one(reversed(line))
+  result_part_one += int(first_digit + last_digit)
+
+print("Part 1:", result_part_one)
+
+
+## Part 2
+from queue import Queue
+
+number_strings = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+def find_number_in_queue(q): # Returns 0 if not found
+  for index, number_string in enumerate(number_strings):
+    number_string_length = len(number_string)
+    if (q.qsize() >= number_string_length
+        and ''.join(list(q.queue)[-number_string_length::]) == number_string):
+      return str(index + 1)
+  return 0
+
+result_part_two = 0
+
+for line in input_lines:
+  q = Queue(5) # the longest number strings have 5 characters
+  numbers = []
+  for char in line:
+    if char.isdigit():
+      numbers.append(char)
+      q = Queue(5)
+    else:
+      if q.full():
+        q.get()
+      q.put(char)
+      number_found = find_number_in_queue(q)
+      if number_found != 0:
+        numbers.append(number_found)
+  result_part_two += int(numbers[0] + numbers[-1])
+
+print("Part 2:", result_part_two)
---
+++ day02/day02.py
@@ -0,0 +1,65 @@
+## Input
+from dataclasses import dataclass
+
+@dataclass
+class CubeReveal:
+  amount: int
+  colour: str
+
+@dataclass
+class Game:
+  id: int
+  reveal_sets: list
+
+with open("input/day02.txt") as f:
+  input = f.read()
+  input_lines = input.splitlines()
+
+games = []
+for index, line in enumerate(input_lines):
+  line = line.split(": ")[1]
+  reveal_sets = []
+  game = Game(index + 1, reveal_sets)
+  games.append(game)
+  for reveal_set in line.split("; "):
+    set = []
+    game.reveal_sets.append(set)
+    for reveal_string in reveal_set.split(", "):
+      amount = int(reveal_string.split(" ")[0])
+      colour = reveal_string.split(" ")[1]
+      set.append(CubeReveal(amount, colour))
+
+
+## Part 1
+capacities = {
+   "red": 12,
+   "green": 13,
+   "blue": 14
+}
+
+result_part_one = 0
+
+for game in games:
+  count_game = True
+  for reveal_set in game.reveal_sets:
+    for reveal in reveal_set:
+      if reveal.amount > capacities[reveal.colour]:
+        count_game = False
+    
+  if count_game:
+    result_part_one += game.id
+
+print("Part 1:", result_part_one)
+
+
+## Part 2
+result_part_two = 0
+
+for game in games:
+  minimum_capacities = {"red": 0, "green": 0, "blue": 0}
+  for reveal_set in game.reveal_sets:
+    for reveal in reveal_set:
+      minimum_capacities[reveal.colour] = max(minimum_capacities[reveal.colour], reveal.amount)
+  result_part_two += minimum_capacities["red"] * minimum_capacities["green"] * minimum_capacities["blue"]
+
+print("Part 2:", result_part_two)
